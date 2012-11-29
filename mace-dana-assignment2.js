ef5ec35830fa7ad6e662a4416a013e34666ff567
Dana Mace <maceface@mac.com>
2012-11-29 18:21:38 -0500

    saved some changes

--- mace-dana-assignment2.js
+++ mace-dana-assignment2.js
@@ -29,7 +29,7 @@ var bestFuelPrice = 6.18
 var getBestFuelPrices = function (fuelPrices) {
 var localAirportPrice = 6.18;
 if (localAirportPrice < fuelPrices) {
-console.log('"For sure," I said, "gas ain\'t gettin\' any cheaper, and I still need to fly to keep up my flight ratings.');
+console.log('"For sure," I said, "gas ain\'t gettin\' any cheaper, and I still need to fly to keep up my flight ratings."');
 } else {
 console.log('"No, I\'m broke." I sadly said.');
 }
@@ -38,9 +38,9 @@ console.log('"No, I\'m broke." I sadly said.');
 //Number Function
 var getInstructorPrice = function (instructorPrice) {
 var aveInstructorPrice = 150;	
-while (instructorPrice > 100) {
+while (instructorPrice >= 100) {
 console.log('I told him, "For you, I\'ll charge ' + instructorPrice + ' dollars an hour." James looked at me for a moment and said, "Will you do it for "' + (aveInstructorPrice - instructorPrice) + '" dollars per hour as a friendly friend price?"');
-instructorPrice-=50;
+instructorPrice-=20;
 }
 var finalInstructorCost = ('I said "sure, I\'ll do it for ' + instructorPrice + ' dollars an hour just for you." You could hear the excitement in his voice, as if he were the one going to be taking lessons!  He looked at me and said, "So when\'s her next flight?!"');
 return finaInstructorCost;
@@ -78,7 +78,7 @@ console.log("I went to the airport yesterday to fly my Cessna 140. The weather w
 console.log('"Will you take her up and see if she likes it enough for me to pay for this?" He asked.  "Gas is pretty expensive nowadays and I do not want to waste money if she is not going to like it anyway"');
 getBestFuelPrices(6.98);
 
-console.log('After taking her up for about an hour, we landed.  I let her stay on the controls with me to get the feel. Her father excitedly ran towards us and asked if she liked it.  After her reply of "YES I LOVED IT", he asked me, "How much would you charge to teach her?"  I thought about it and said, "I will charge you $150 an hour."');
+console.log('After taking her up for about an hour, we landed.  I let her stay on the controls with me to get the feel. Her father excitedly ran towards us and asked if she liked it.  After her reply of "YES I LOVED IT", he asked me, "How much would you charge to teach her?"  I thought to myself, "I will charge $150 an hour, maybe a little less for a friend, but not much."');
 var finaInstructorCost = getInstructorPrice(150);
 console.log(finaInstructorCost);
 
