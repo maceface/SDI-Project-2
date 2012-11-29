2a84819af42acaf95d226ff3dc4afabe30cb1dec
Dana Mace <maceface@mac.com>
2012-11-29 16:55:25 -0500

    number function

--- mace-dana-assignment2.js
+++ mace-dana-assignment2.js
@@ -35,8 +35,22 @@ console.log('"No, I\'m broke." I sadly said.');
 }
 };
 
+//Number Function
+var getInstructorPrice = function (instructorPrice) {
+var aveInstructorPrice = 150;	
+while (instructorPrice > 100) {
+console.log('I told him, "For you, I\'ll charge ' + instructorPrice + ' dollars an hour." Bob looked at me for a moment and said, "Will you do it for "' + (aveInstructorPrice - instructorPrice) + '" dollars per hour as a friendly friend price?"');
+instructorPrice-=50;
+}
+var finalInstructorCost = ('I said "sure, I\'ll do it for ' + instructorPrice + ' dollars an hour just for you." You could hear the excitement in his voice, as if he were the one going to be taking lessons!  He looked at me and said, "So when\'s her next flight?!"');
+return finaInstructorCost;
+};
+
+
 //OUTPUT
 console.log("I went to the airport yesterday to fly my Cessna 140. The weather was perfect. James, a friend of mine asked if I was available to give his 13 year old daughter a flight lesson.");
 console.log('"Will you take her up and see if she likes it enough for me to pay for this?" He asked.  "Gas is pretty expensive nowadays and I do not want to waste money if she is not going to like it anyway"');
 airportWithOkayPricedFuel(6.98);
+
+
 
