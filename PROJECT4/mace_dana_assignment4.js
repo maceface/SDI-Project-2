3c30800d0946eaf92265b388de5cc412b05149a9
Dana Mace <maceface@mac.com>
2012-12-11 20:51:30 -0500

    on to the numbers

--- PROJECT4/mace_dana_assignment4.js
+++ PROJECT4/mace_dana_assignment4.js
@@ -43,15 +43,59 @@ var myLibrary = function(){
     };
     
  
-    //Title-case a string (split into words, then uppercase the first letter of each word)
+    //4.  Title-case a string (split into words, then uppercase the first letter of each word)
+    
+    var split = function(splitTitleCase) {
+    
+    var regex = /\w\S*/g;
+    
+    var titleCaseString = function(splitTitleCase) {
+    
+    var process = splitTitleCase.charAt(0).toUpperCase() + splitTitleCase.substr(1);
+        
+        return process;
+    };
+
+    var stringChanged = splitTitleCase.replace(regex, titleCaseString);
+        
+        return stringChanged;	
+    };
+    
+    
+    /*5.  Given a string that is a list of things separated by a given string, as well as another
+    string separator, return a string with the first separator changed to the
+    second: "a,b,c" + "," + "/" ? "a/b/c".
+    */
+    
+    changeStringSeparator = function(string, separator1, separator2) {
+
+        var splitString = string.split(separator1);
+
+        var reunitedString = splitString.join(separator2);
+
+        return reunitedString;
+    };
+    
+    
+//.............NUMBERS..............
+
+    //6.  Format a number to use a specific number of decimal places, as for money: 2.1 to 2.10
+    
     
     
 //RETURNS    
     
      return {
+        
+        //STRINGS
         "checkPhoneNum" : checkPhoneNum,
         "checkEmailAddy" : checkEmailAddy,
         "checkURL" : checkURL,
+        "split" : split,
+        "changeStringSeparator" : changeStringSeparator,
+        
+        //NUMBERS
+        
     };
     
 };
@@ -71,6 +115,15 @@ console.log("That's an email pattern... " + newLib.checkEmailAddy("maceface@mac.
     
     //URL
 console.log("It is " + newLib.checkURL("www.maceface@mac.com") + " that this is a URL because the existance of the http:// or https:// is " + newLib.checkURL("www.maceface@mac.com") + ".");
+    // SPLIT STRING
+console.log("my name is..." + newLib.split("dana noelle mace"));
+
+    //STRING SEPARATOR
+console.log('At the click of a button, we can change "a|b|c" to ' + changeStringSeparator("a|b|c","|","-"));
+
+    //
+
+
 
 
 
