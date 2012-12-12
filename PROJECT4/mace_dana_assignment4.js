c59e7679ecc0a3ef4b797454dd2f20cba700b4ca
Dana Mace <maceface@mac.com>
2012-12-11 20:19:57 -0500

    1-4.5

--- PROJECT4/mace_dana_assignment4.js
+++ PROJECT4/mace_dana_assignment4.js
@@ -16,22 +16,61 @@ var myLibrary = function(){
             return true;
         } else {
             return false;
-        }
+        };
     };
     
-    return {
+    
+
+    //2.  Does a string follow an aaa@bbb.ccc pattern like an email address?
+    
+    var checkEmailAddy = function (emailAddy) {
+        
+        if (emailAddy.match(/^[a-z0-9\._-]+@([a-z0-9_-]+\.)+[a-z]{2,6}$/i)) {
+            return true;
+        } else {
+            return false;
+        };
+    };
+    
+    
+    //3.  Is the string a URL? (Does it start with http: or https:?)
+    
+    var checkURL = function(uRL) {
+    var regex = /^(http|https):/; //Regex: Check for http or https within the string.
+    var checkString = regex.test(uRL);
+
+        return checkString;
+    };
+    
+ 
+    //Title-case a string (split into words, then uppercase the first letter of each word)
+    
+    
+//RETURNS    
+    
+     return {
         "checkPhoneNum" : checkPhoneNum,
+        "checkEmailAddy" : checkEmailAddy,
+        "checkURL" : checkURL,
     };
+    
 };
-
-
-
-var newLib = new Library();
-
-console.log("Is this a phone number? " + newLib.checkPhoneNum("888-333-1111"));
     
 
+
+//OUTPUTS
     
+var newLib = new myLibrary();
+
+    //STRINGS
+    //PHONE NUMBER
+console.log("This a phone number pattern... " + newLib.checkPhoneNum("888-333-1111"));
+    
+    //EMAIL ADDRESS
+console.log("That's an email pattern... " + newLib.checkEmailAddy("maceface@mac.com"));
+    
+    //URL
+console.log("It is " + newLib.checkURL("www.maceface@mac.com") + " that this is a URL because the existance of the http:// or https:// is " + newLib.checkURL("www.maceface@mac.com") + ".");
 
 
 
